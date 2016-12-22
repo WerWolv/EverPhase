df3329d94c32145645adb5798aa01571c67839d4
WerWolv98 <werwolv98@gmail.com>
2016-12-22 20:52:28 +0100

    Fixed coding style

--- src/com/werwolv/input/KeyListener.java
+++ src/com/werwolv/input/KeyListener.java
@@ -5,11 +5,16 @@ import static org.lwjgl.glfw.GLFW.*;
 
 public class KeyListener extends GLFWKeyCallback {
 
-    public static boolean[] keys = new boolean[1024];
+    private static boolean[] keys = new boolean[1024];
 
     @Override
     public void invoke(long window, int key, int scancode, int action, int mods) {
         keys[key] = action != GLFW_RELEASE;
     }
 
+
+    public static boolean isKeyPressed(int key) {
+        if(key < 0 || key >= 1024) return false;
+        return keys[key];
+    }
 }
--- src/com/werwolv/terrain/Terrain.java
+++ src/com/werwolv/terrain/Terrain.java
@@ -25,28 +25,28 @@ public class Terrain {
         int count = VERTEX_CNT * VERTEX_CNT;
         float[] vertices = new float[count * 3];
         float[] normals = new float[count * 3];
-        float[] textureCoords = new float[count*2];
-        int[] indices = new int[6*(VERTEX_CNT-1)*(VERTEX_CNT-1)];
+        float[] textureCoords = new float[count * 2];
+        int[] indices = new int[6 * (VERTEX_CNT - 1) * (VERTEX_CNT - 1)];
         int vertexPointer = 0;
         for(int i = 0; i < VERTEX_CNT; i++){
             for(int j = 0; j < VERTEX_CNT; j++){
-                vertices[vertexPointer*3] = (float)j/((float)VERTEX_CNT - 1) * SIZE;
-                vertices[vertexPointer*3+1] = 0;
-                vertices[vertexPointer*3+2] = (float)i/((float)VERTEX_CNT - 1) * SIZE;
-                normals[vertexPointer*3] = 0;
-                normals[vertexPointer*3+1] = 1;
-                normals[vertexPointer*3+2] = 0;
-                textureCoords[vertexPointer*2] = (float)j/((float)VERTEX_CNT - 1);
-                textureCoords[vertexPointer*2+1] = (float)i/((float)VERTEX_CNT - 1);
+                vertices[vertexPointer * 3] = (float) j /((float) VERTEX_CNT - 1) * SIZE;
+                vertices[vertexPointer * 3 + 1] = 0;
+                vertices[vertexPointer * 3 + 2] = (float) i /((float) VERTEX_CNT - 1) * SIZE;
+                normals[vertexPointer * 3] = 0;
+                normals[vertexPointer * 3 + 1] = 1;
+                normals[vertexPointer * 3 + 2] = 0;
+                textureCoords[vertexPointer * 2] = (float) j /((float) VERTEX_CNT - 1);
+                textureCoords[vertexPointer *2 + 1] = (float) i /((float) VERTEX_CNT - 1);
                 vertexPointer++;
             }
         }
         int pointer = 0;
-        for(int gz=0;gz<VERTEX_CNT-1;gz++){
-            for(int gx=0;gx<VERTEX_CNT-1;gx++){
-                int topLeft = (gz*VERTEX_CNT)+gx;
+        for(int gz = 0; gz < VERTEX_CNT-1; gz++){
+            for(int gx = 0; gx<VERTEX_CNT - 1; gx++){
+                int topLeft = (gz * VERTEX_CNT) + gx;
                 int topRight = topLeft + 1;
-                int bottomLeft = ((gz+1)*VERTEX_CNT)+gx;
+                int bottomLeft = ((gz + 1) * VERTEX_CNT) + gx;
                 int bottomRight = bottomLeft + 1;
                 indices[pointer++] = topLeft;
                 indices[pointer++] = bottomLeft;
