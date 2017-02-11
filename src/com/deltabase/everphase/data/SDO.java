4ea7fc17f793dc37ead1a52f239e20569bfee746
WerWolv98 <werwolv98@gmail.com>
2017-02-11 13:45:30 +0100

    Started working on an object serializer

---
+++ src/com/deltabase/everphase/data/SDO.java
@@ -0,0 +1,17 @@
+package com.deltabase.everphase.data;
+
+import java.io.Serializable;
+
+public abstract class SDO implements Serializable {
+
+    private transient String filePath = "";
+
+    public void serialize() {
+
+    }
+
+    public void deserialize() {
+
+    }
+
+}
